101c550e0a30d4eefb83c386e7bb46bbb1557fea
Tyler Axdorff <Tyler.Axdorff@outlook.com>
2024-08-31 11:54:14 -0700

    Fallback to default python when no venv

--- .config/nvim/lua/taxdorff/plugins/dap/nvim-dap-python.lua
+++ .config/nvim/lua/taxdorff/plugins/dap/nvim-dap-python.lua
@@ -6,9 +6,15 @@ return {
         "rcarriga/nvim-dap-ui",
     },
     config = function()
-        -- local path = "~/.local/share/nvim/mason/packages/debugpy/venv/bin/python"
-        local path = os.getenv("VIRTUAL_ENV") .. "/bin/python"
-        -- local path = '~/.virtualenvs/debugpy/bin/python'
+        local venv = os.getenv("VIRTUAL_ENV")
+        local path
+        if not venv then
+            -- If no virtual_env dir, use a default python
+            path = "~/.local/share/nvim/mason/packages/debugpy/venv/bin/python"
+            -- local path = '~/.virtualenvs/debugpy/bin/python'
+        else
+            path = venv .. "/bin/python"
+        end
         require("dap-python").setup(path)
         require("dap-python").test_runner = 'pytest'
         vim.keymap.set('n', '<leader>dpr', function()
